1b9b4bbc070334fdfb8e6157bd644ea6c584f417
Richard Slater <richard.slater@amido.com>
2015-05-20 14:28:07 +0100

    Adding Pre-processed File to NuGet

--- Amido.Net.Http.Formatting.YamlMediaTypeFormatter/Content/App_Start/YamlMediaTypeFormatter.cs.pp
+++ Amido.Net.Http.Formatting.YamlMediaTypeFormatter/Content/App_Start/YamlMediaTypeFormatter.cs.pp
@@ -1,10 +1,11 @@
 using System;
+using System.Web.Http;
 
 [assembly: WebActivatorEx.PreApplicationStartMethod(
-    typeof($rootnamespace$.App_Start.YamlMediaTypeFormatter), "PreStart")]
+    typeof($rootnamespace$.App_Start.YamlMediaTypeFormatterConfig), "PreStart")]
 
 namespace $rootnamespace$.App_Start {
-    public static class YamlMediaTypeFormatter {
+    public static class YamlMediaTypeFormatterConfig {
         public static void PreStart() {
             GlobalConfiguration.Configuration.Formatters.Add(new YamlMediaTypeFormatter());
         }
