c13b6c18f23173d6a313a26677aa8249eeb077aa
Richard Slater <richard.slater@amido.com>
2015-05-21 20:34:28 +0100

    Enabled SharedAssemblyInfo.

--- Solutions/Amido.Net.Http.Formatting.YamlMediaTypeFormatter.Samples.NetFX46/Properties/AssemblyInfo.cs
+++ Solutions/Amido.Net.Http.Formatting.YamlMediaTypeFormatter.Samples.NetFX46/Properties/AssemblyInfo.cs
@@ -2,34 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Amido.Net.Http.Formatting.YamlMediaTypeFormatter.Samples.NetFX46")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Amido.Net.Http.Formatting.YamlMediaTypeFormatter.Samples.NetFX46")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("81deda20-6c31-42ad-825f-40853c542df3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
