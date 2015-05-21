4cbe5f2f50d192a4ecd4da0043427ce1f8df5698
Richard Slater <richard.slater@amido.com>
2015-05-21 21:04:56 +0100

    Updated Script to New Structure

--- Solutions/Amido.Net.Http.Formatting.YamlMediaTypeFormatter/Properties/AssemblyInfo.cs
+++ Solutions/Amido.Net.Http.Formatting.YamlMediaTypeFormatter/Properties/AssemblyInfo.cs
@@ -2,8 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("YamlMediaTypeFormatter")]
-[assembly: AssemblyDescription("")]
-
-
+[assembly: AssemblyTitle("ASP.NET Web API YAML MediaTypeFormatter")]
+[assembly: AssemblyDescription("Provides a MediaTypeFormatter to allow ASP.NET Web API projects to accept and emit YAML as well as JSON and XML.")]
 [assembly: Guid("4c6bc555-04b3-4e58-8615-b5a1c781860e")]
--- default.ps1
+++ default.ps1
@@ -1,21 +1,23 @@
 Framework 4.6
 
 properties {
-  $packageVersion = "1.0.$((Get-Date).Year - 2000)$((Get-Date).DayOfYear).$([Math]::Floor(((Get-Date) - (Get-Date).Date).TotalSeconds))";
+  $assemblyVersion = 0;
+  $packageVersion = "1.0.0.{0}" -f $assemblyVersion;
   $toolsVersion = "14.0";
   $buildConfiguration = "Release";
+  $solution = "Solutions/Amido.Net.Http.Formatting.YamlMediaTypeFormatter.sln";
 }
 
 task default -depends Compile
 
-task Compile -depends Clean { 
-  msbuild Amido.Net.Http.Formatting.YamlMediaTypeFormatter.sln /p:VisualStudioVersion=$toolsVersion /p:Configuration=$buildConfiguration
+task Clean {
+  msbuild $solution /m /t:clean /p:VisualStudioVersion=$toolsVersion /p:Configuration=$buildConfiguration
 }
 
-task Clean { 
-  msbuild Amido.Net.Http.Formatting.YamlMediaTypeFormatter.sln /t:clean /p:VisualStudioVersion=$toolsVersion /p:Configuration=$buildConfiguration
+task Compile -depends Clean { 
+  msbuild $solution /m /p:VisualStudioVersion=$toolsVersion /p:Configuration=$buildConfiguration
 }
 
 task Pack -depends Compile {
-  & tools/nuget.exe pack "Amido.Net.Http.Formatting.YamlMediaTypeFormatter/Amido.Net.Http.Formatting.YamlMediaTypeFormatter.nuspec" -OutputDirectory "artefacts" -Version $packageVersion
+  & tools/nuget.exe pack "Solutions/Amido.Net.Http.Formatting.YamlMediaTypeFormatter/Amido.Net.Http.Formatting.YamlMediaTypeFormatter.nuspec" -OutputDirectory "Artefacts" -Version $packageVersion
 }
