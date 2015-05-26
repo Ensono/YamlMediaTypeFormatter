17b7bab41f2b6ce5dad203dd71f70b1f3306f00b
Richard Slater <richard.slater@amido.com>
2015-05-26 16:18:10 +0100

    Added Test Project

---
+++ Solutions/Amido.Net.Http.Formatting.YamlMediaTypeFormatter.Tests/YamlMediaTypeFormatterTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Xunit;
+
+namespace Amido.Net.Http.Formatting.YamlMediaTypeFormatter.Tests {
+    public class YamlMediaTypeFormatterTests {
+        [Fact]
+        public void PublicPropertiesInitialized() {
+            var formatter = new YamlMediaTypeFormatter();
+            Assert.False(formatter.IgnoreUnmatched);
+        }
+    }
+}
