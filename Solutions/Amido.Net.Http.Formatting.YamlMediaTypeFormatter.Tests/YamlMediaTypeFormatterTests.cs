ff01a5c6dd12732f88cb1daf43fc4b191850a2d5
Richard Slater <richard.slater@amido.com>
2015-05-29 08:39:36 +0100

    Added Test for YAML Naming Convention
    Added Test for the public NamingConvention property to expect the default to be the CamelCaseNamingConvention.

--- Solutions/Amido.Net.Http.Formatting.YamlMediaTypeFormatter.Tests/YamlMediaTypeFormatterTests.cs
+++ Solutions/Amido.Net.Http.Formatting.YamlMediaTypeFormatter.Tests/YamlMediaTypeFormatterTests.cs
@@ -4,12 +4,15 @@ using System.Linq;
 
 using Xunit;
 
+using YamlDotNet.Serialization.NamingConventions;
+
 namespace Amido.Net.Http.Formatting.YamlMediaTypeFormatter.Tests {
     public class YamlMediaTypeFormatterTests {
         [Fact]
         public void PublicPropertiesInitialized() {
             var formatter = new YamlMediaTypeFormatter();
             Assert.False(formatter.IgnoreUnmatched);
+            Assert.IsType<CamelCaseNamingConvention>(formatter.NamingConvention); 
         }
     }
 }
